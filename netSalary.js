ee1c00df49a4e38294b3d789217632d6ad7a2aea
membae <membaphilip1@gmail.com>
2024-07-01 12:14:54 +0300

    last submission

--- netSalary.js
+++ netSalary.js
@@ -1,40 +1,38 @@
-const prompt = require('prompt-sync')
-
-function salaryCalculator() {
-    let salary = prompt("Please enter the salary:");
-    salary = parseInt(salary);
-
-    if (isNaN(salary)) {
-        console.log("Invalid input. Please enter a numeric value for the salary.");
-        return;
-    }
-
+const read=require('readline').Interface({
+    input:process.stdin,
+    output:process.stdout
+  });
+  read.question('enter your salary?', salary=>{
+     salo=Number(salary);
+    const allowance=100000
     let paye, nssf, nhif, netPay;
-
-    if (salary <= 24000) {
-        paye = salary * 0.1;
-        nssf = salary * 0.06;
-        nhif = 750;
-    } else if (salary <= 32333) {
-        paye = salary * 0.25;
-        nssf = salary * 0.06;
-        nhif = 1000;
-    } else if (salary <= 50000) {
-        paye = salary * 0.3;
-        nssf = salary * 0.06;
-        nhif = 1700;
-    } else if (salary <= 80000) {
-        paye = salary * 0.325;
-        nssf = salary * 0.06;
-        nhif = 1700;
-    } else {
-        paye = salary * 0.35;
-        nssf = salary * 0.06;
-        nhif = 1000;
-    }
-
-    netPay = salary - paye - nssf - nhif;
-    console.log(`Your net salary is ${netPay}`);
-}
-
-salaryCalculator();
+  
+      if (salo <= 24000) {
+          paye = salo * 0.1;
+          nssf = salo * 0.06;
+          nhif = 750;
+      } else if (salo <= 32333) {
+          paye = salo * 0.25;
+          nssf = salo * 0.06;
+          nhif = 1000;
+      } else if (salo <= 50000) {
+          paye = salo * 0.3;
+          nssf = salo * 0.06;
+          nhif = 1700;
+      } else if (salo <= 80000) {
+          paye = salo * 0.325;
+          nssf = salo * 0.06;
+          nhif = 1700;
+      } else {
+          paye = salo * 0.35;
+          nssf = salo * 0.06;
+          nhif = 1000;
+      }
+  
+      netPay = salo - paye - nssf - nhif;
+      console.log(`Your net salary is ${netPay}`);
+      read.close();
+  }
+  
+    //
+  );
